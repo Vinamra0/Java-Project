e8e04e043bb1ddd004530d1332785ff63bfebf61
Vinamra Singh <singh001vinamra@gmail.com>
2020-10-31 12:48:27 +0530

    first commit

---
+++ src/sample/Main.java
@@ -0,0 +1,61 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import javax.swing.*;
+import java.sql.*;
+
+public class Main extends Application {
+
+    public static int height = 1000;
+    public static int width = 600;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+
+        Parent login = FXMLLoader.load(getClass().getResource("login.fxml"));
+
+        Scene scene = new Scene(login, this.height, this.width);
+
+        primaryStage.setTitle("JEE Management System");
+
+
+
+
+        primaryStage.setScene(scene);
+
+        Rectangle2D screenBounds = Screen.getPrimary().getBounds(); //  getting displayInfo
+
+        //  setting position of window at center
+        primaryStage.setX((screenBounds.getMaxX()-this.height)/2);
+        primaryStage.setY((screenBounds.getMaxY()-this.width)/2);
+
+
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) throws SQLException {
+        Connection conn = databaseConnection.connect();
+        Statement stmt = conn.createStatement();
+
+        //  making students table if it does not exist
+        String sql = "CREATE TABLE IF NOT EXISTS students (rollnumber INTEGER PRIMARY KEY AUTOINCREMENT, password VARCHAR(25) NOT NULL , studentName TEXT NOT NULL, fName TEXT NOT NULL, mName TEXT NOT NULL, sex TEXT NOT NULL, category TEXT NOT NULL, dobD INTEGER NOT NULL, dobM TEXT NOT NULL, dobY INTEGER NOT NULL);";
+        stmt.execute(sql);
+
+        //  making marks table if does not exist
+        sql = "CREATE TABLE IF NOT EXISTS marks (rollnumber INTEGER NOT NULL, maths INTEGER DEFAULT 0 CHECK ( maths<=360 ), physics INTEGER DEFAULT 0 CHECK ( physics<=360 ), chemistry INTEGER DEFAULT 0 CHECK ( chemistry<=360 ),FOREIGN KEY (rollnumber) REFERENCES students(rollnumber));";
+        stmt.execute(sql);
+
+        stmt.closeOnCompletion();
+        launch(args);
+    }
+}
---
+++ src/sample/loginController.java
@@ -0,0 +1,40 @@
+package sample;
+
+import com.jfoenix.controls.*;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+
+import javax.swing.*;
+import java.io.IOException;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Arrays;
+import java.util.ResourceBundle;
+
+public class loginController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    public void loginBTNclick(ActionEvent actionEvent) {
+        System.out.println("login controller click");
+    }
+}
